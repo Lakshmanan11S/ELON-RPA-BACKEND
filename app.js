60752bdab0893cdd5b3fbca1d6a9331b09375192
Lakshmanan11S <lakshmanan@elonnnativesystem.com>
2024-11-11 22:37:12 +0530

    update

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const  cors = require('cors');
 const  bodyParser = require('body-parser');
-const PORT = 8000;
+const PORT = 7000;
 const app = express()
 require('dotenv').config();
 const router = require('./Router')
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATA_BASE)
+.then(()=>{console.log("Db connected")})
+.catch((error)=>{console.log("Db not connected",error)})
+
+
 
 app.use(express.json());
 app.use(bodyParser.json());
--- automationProcess.js
+++ automationProcess.js
@@ -91,11 +91,108 @@
 
 
 
+// const puppeteer = require('puppeteer');
+// const sleep =(ms)=>new Promise((resolve)=>setTimeout(resolve,ms))
+// const path = require('path')
+// const DOWNLOAD_PATH = path.join(__dirname,'downloads')
+// const fs = require('fs').promises;
+
+// exports.automationProcess=async(req,res)=>{
+//     const {first_name, last_name, dob, document_type, afterDate, beforeDate, emr_url, request_id}=req.body
+    
+//     try{
+//         const browser = await puppeteer.launch({headless:false});
+//         const page = await browser.newPage();
+//         const client = await page.target().createCDPSession();
+//         await client.send('Page.setDownloadBehavior',{
+//             behavior:'allow',
+//             downloadPath:DOWNLOAD_PATH,
+//         })
+//         await page.setViewport({height:750,width:1500});
+//         await page.goto(emr_url);
+//         await page.type('#email',process.env.USERNAMEA,1000);
+//         await sleep(2000);
+//         await page.type('#password',process.env.PASSWORDA,1000);
+//         await sleep(2000);
+//         await page.click('#submit-login');
+//         await sleep(20000);
+//         await page.click('#dashboardHeaderRpm');
+//         await sleep(20000);
+//         await page.click('span.px-3.border-radius-10px.box-inset-shadow.card-value.heading-2.click-to-navigate-text')
+//         await sleep(10000);
+//         // await page.click('tr.graph-color > th.table-user-row.text-left.sticky-table-cells');
+
+//         await page.evaluate(() => {
+//             const rows = document.querySelectorAll('th');
+//             rows.forEach(row => {
+//                 if (row.textContent.includes("Miss. Abigail Tay")) {
+//                     row.style.backgroundColor = "yellow"; 
+//                     row.setAttribute('data-click-target', 'true');
+//                 }
+//             });
+//         });
+//         await sleep(2000)
+//         await page.click('th[data-click-target="true"]');
+//         await sleep(5000);
+//         await page.click('#ccmReportDownloadIcon');
+//         await sleep(2000);
+//         await page.click('button.button2.buttonOkBackground.btn.btn-primary');
+//         await sleep(60000);
+//         await page.click('div.home-image.my-auto');
+//         await sleep(3000)
+//         await page.click('button.button2.button-popup-cancel.btn.btn-primary');
+//         await sleep(2000)
+//         await page.keyboard.press('Enter');
+//         await sleep(2000);
+//         await page.click('div.d-flex.my-auto.mr-2');
+//         await sleep(3000);
+
+//         const files = await fs.readdir(DOWNLOAD_PATH);
+//         const pdfRecord = files.find(file =>file.includes('CCM_Report'));
+//         const excelRecord = files.find(file =>file.includes('Dashboard'))
+//         const base64PdfRecord = pdfRecord? await convertPdfFileToBase64(path.join(DOWNLOAD_PATH,pdfRecord)):null;
+//         const base64ExcelRecord = excelRecord? await convertExcelFileToBase64(path.join(DOWNLOAD_PATH,excelRecord)):null;
+//         await browser.close();
+//         return res.status(200).json({
+//             Status:"True",
+//             Code:"R-200 Successfully Retrieval",
+//             message:"All Record Retrieval  successfully",
+//             pdfFile :base64PdfRecord,
+//             excelFile :base64ExcelRecord
+//         })
+
+//     }catch(error){
+//         console.log(error)
+//          res.status(500).json({message:"Internal Server Error",error})
+//     }
+// }
+// const convertPdfFileToBase64 = async (filePath)=>{
+//     const fileData = await fs.readFile(filePath);
+//     const base64Data =  fileData.toString('base64');
+//     await fs.unlink(filePath);
+//     return base64Data
+// }
+// const convertExcelFileToBase64 = async (filePath)=>{
+//     const fileData = await fs.readFile(filePath);
+//     const base64Data =  fileData.toString('base64');
+//     // await fs.unlink(filePath);
+//     return base64Data
+// }
+
+
+
+
 const puppeteer = require('puppeteer');
 const sleep =(ms)=>new Promise((resolve)=>setTimeout(resolve,ms))
 const path = require('path')
 const DOWNLOAD_PATH = path.join(__dirname,'downloads')
 const fs = require('fs').promises;
+const xlsx = require('xlsx');
+const { MongoClient } = require('mongodb');
+
+const mongoUri = process.env.DATA_BASE; 
+const dbName = "proclee";
+const collectionName ="data";
 
 exports.automationProcess=async(req,res)=>{
     const {first_name, last_name, dob, document_type, afterDate, beforeDate, emr_url, request_id}=req.body
@@ -120,7 +217,7 @@ exports.automationProcess=async(req,res)=>{
         await sleep(20000);
         await page.click('span.px-3.border-radius-10px.box-inset-shadow.card-value.heading-2.click-to-navigate-text')
         await sleep(10000);
-        // await page.click('tr.graph-color > th.table-user-row.text-left.sticky-table-cells');
+        // // await page.click('tr.graph-color > th.table-user-row.text-left.sticky-table-cells');
 
         await page.evaluate(() => {
             const rows = document.querySelectorAll('th');
@@ -133,7 +230,7 @@ exports.automationProcess=async(req,res)=>{
         });
         await sleep(2000)
         await page.click('th[data-click-target="true"]');
-        await sleep(5000);
+        await sleep(10000);
         await page.click('#ccmReportDownloadIcon');
         await sleep(2000);
         await page.click('button.button2.buttonOkBackground.btn.btn-primary');
@@ -152,6 +249,13 @@ exports.automationProcess=async(req,res)=>{
         const excelRecord = files.find(file =>file.includes('Dashboard'))
         const base64PdfRecord = pdfRecord? await convertPdfFileToBase64(path.join(DOWNLOAD_PATH,pdfRecord)):null;
         const base64ExcelRecord = excelRecord? await convertExcelFileToBase64(path.join(DOWNLOAD_PATH,excelRecord)):null;
+       
+        if (excelRecord) {
+            const excelFilePath = path.join(DOWNLOAD_PATH, excelRecord);
+            const jsonData = await convertExcelToJson(excelFilePath);
+            await saveDataToMongo(jsonData); 
+        }
+
         await browser.close();
         return res.status(200).json({
             Status:"True",
@@ -175,6 +279,52 @@ const convertPdfFileToBase64 = async (filePath)=>{
 const convertExcelFileToBase64 = async (filePath)=>{
     const fileData = await fs.readFile(filePath);
     const base64Data =  fileData.toString('base64');
-    await fs.unlink(filePath);
     return base64Data
 }
+
+const convertExcelToJson = async (filePath) => {
+    const workbook = xlsx.readFile(filePath);
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const rawData = xlsx.utils.sheet_to_json(worksheet, { header: 1, range: 7 }); 
+    const jsonData = []; 
+    rawData.forEach((row, rowIndex) => {
+        const rowData = {};
+        if (row[0]) rowData["Name"] = row[0];
+        if (row[1]) rowData["EnrollingPhysicianName"] = row[1];
+        if (row[2]) rowData["CTMName"] = row[2];
+        if (row[3]) rowData["EffortsSpentByDr.AgniPathak"] = row[3];
+        if (row[4]) rowData["TotalTimeSpent"] = row[4];
+        if (row[5]) rowData["CallCompliance/No. of days_since_lastcheckin"] = row[5];
+        if (row[6] != null && row[6] !== "") rowData["CallCompliance/No. of attempts"] = row[6];
+        if (row[7]) rowData["99490/Check_in_Status"] = row[7];
+        if (row[8]) rowData["99490/Billing Readiness"] = row[8];
+        if (row[9]) rowData["99439/Check_in_Status"] = row[9];
+        if (row[10]) rowData["99439/Billing Readiness"] = row[10];
+        if (row[11]) rowData["Notes"] = row[11];
+                     
+        if (Object.keys(rowData).length > 0) {
+            jsonData.push(rowData);
+        }
+    });
+    await fs.unlink(filePath);
+
+    return jsonData;
+};
+
+
+const saveDataToMongo = async (jsonData) => {
+    const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        await collection.insertMany(jsonData);
+        console.log("Excel data saved to MongoDB in JSON format");
+    } catch (error) {
+        console.error("Error saving data to MongoDB:", error);
+    } finally {
+        await client.close();
+    }
+};
